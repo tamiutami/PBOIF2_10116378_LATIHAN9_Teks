daea2c2e5d6a706713393a583acd5514a2e3801c
sriutaminingsih <49768122+tamiutami@users.noreply.github.com>
2020-11-05 03:01:35 +0700

    Add files via upload

---
+++ teks/src/teks/Teks.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package teks;
+
+/**
+ *
+ * @author Sri Utami Ningsih
+ * Nama  : Sri Utami Ningsih
+ * Kelas :PBO-IF2
+ * Nim   : 10116378
+ */
+public class Teks {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        char c;
+        char tab = '\t';
+        char newline = '\n';
+        c = 'A';
+        System.out.println("Hasil" + newline + "Char: " + tab + "berisi " + c);
+    }
+    
+}
